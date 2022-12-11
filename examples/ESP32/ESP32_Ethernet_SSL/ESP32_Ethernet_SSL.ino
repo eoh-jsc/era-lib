08465b2936512745a764a0c32a65cd2cd176d708
Quoc Hung <hung.pbq@eoh.io>
2022-12-11 20:13:18 +0700

    Update ESP32 Ethernet examples

---
+++ examples/ESP32/ESP32_Ethernet_SSL/ESP32_Ethernet_SSL.ino
@@ -0,0 +1,47 @@
+/*************************************************************
+  Download latest ERa library here:
+    https://github.com/eoh-jsc/era-lib/releases/latest
+    https://www.arduino.cc/reference/en/libraries/era
+    https://registry.platformio.org/libraries/eoh-ltd/ERa/installation
+
+    ERa website:                https://e-ra.io
+    ERa blog:                   https://iotasia.org
+    ERa forum:                  https://forum.eoh.io
+    Follow us:                  https://www.fb.com/EoHPlatform
+ *************************************************************/
+
+// Enable debug console
+// Set CORE_DEBUG_LEVEL = 3 first
+// #define ERA_DEBUG
+
+// You should get Auth Token in the ERa App or ERa Dashboard
+#define ERA_AUTH_TOKEN "ERA2706"
+
+#include <Arduino.h>
+#include <ERaEsp32SSLEthernet.hpp>
+#include <ERa/ERaTimer.hpp>
+
+const int pwrPin = 14;
+const uint8_t phyAddr = 1;
+
+ERaTimer timer;
+
+/* This function print uptime every second */
+void timerEvent() {
+    ERA_LOG("Timer", "Uptime: %d", ERaMillis() / 1000L);
+}
+
+void setup() {
+    /* Setup debug console */
+    Serial.begin(115200);
+
+    ERa.begin(phyAddr, pwrPin);
+
+    /* Setup timer called function every second */
+    timer.setInterval(1000L, timerEvent);
+}
+
+void loop() {
+    ERa.run();
+    timer.run();
+}
