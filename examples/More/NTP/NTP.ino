6940d2232be00c02cea8e5521c6520ba901cd357
Quốc Hùng <hung.pbq@eoh.io>
2024-06-09 23:36:24 +0700

    Bump version to v1.3.2 * Add ntp esp...

---
+++ examples/More/NTP/NTP.ino
@@ -0,0 +1,76 @@
+/*************************************************************
+  Download latest ERa library here:
+    https://github.com/eoh-jsc/era-lib/releases/latest
+    https://www.arduino.cc/reference/en/libraries/era
+    https://registry.platformio.org/libraries/eoh-ltd/ERa/installation
+
+    ERa website:                https://e-ra.io
+    ERa blog:                   https://iotasia.org
+    ERa forum:                  https://forum.eoh.io
+    Follow us:                  https://www.fb.com/EoHPlatform
+ *************************************************************/
+
+// Enable debug console
+// Set CORE_DEBUG_LEVEL = 3 first
+// #define ERA_DEBUG
+// #define ERA_SERIAL Serial
+
+/* Select ERa host location (VN: Viet Nam, SG: Singapore) */
+#define ERA_LOCATION_VN
+// #define ERA_LOCATION_SG
+
+// You should get Auth Token in the ERa App or ERa Dashboard
+#define ERA_AUTH_TOKEN "ERA2706"
+
+#include <Arduino.h>
+#include <ERa.hpp>
+#include <Time/ERaEspTime.hpp>
+
+const char ssid[] = "YOUR_SSID";
+const char pass[] = "YOUR_PASSWORD";
+
+/* NTP Time */
+ERaEspTime syncTime;
+TimeElement_t ntpTime;
+
+/* This function will run every time ERa is connected */
+ERA_CONNECTED() {
+    syncTime.begin();
+    ERA_LOG("ERa", "ERa connected!");
+}
+
+/* This function will run every time ERa is disconnected */
+ERA_DISCONNECTED() {
+    ERA_LOG("ERa", "ERa disconnected!");
+}
+
+/* This function print uptime every second */
+void timerEvent() {
+    syncTime.getTime(ntpTime);
+    ERA_LOG(ERA_PSTR("NTP"), ERA_PSTR("%02d:%02d:%02dT%02d-%02d-%04dW%d"),
+                             ntpTime.hour, ntpTime.minute, ntpTime.second,
+                             ntpTime.day, ntpTime.month, ntpTime.year + 1970, ntpTime.wDay);
+}
+
+void setup() {
+    /* Setup debug console */
+#if defined(ERA_DEBUG)
+    Serial.begin(115200);
+#endif
+
+    /* Set board id */
+    // ERa.setBoardID("Board_1");
+
+    /* Config timezone +7 */
+    syncTime.config(7);
+
+    /* Initializing the ERa library. */
+    ERa.begin(ssid, pass);
+
+    /* Setup timer called function every second */
+    ERa.addInterval(1000L, timerEvent);
+}
+
+void loop() {
+    ERa.run();
+}
---
+++ src/Time/ERaEspTime.hpp
@@ -0,0 +1,75 @@
+#ifndef INC_ERA_ESP_TIME_HPP_
+#define INC_ERA_ESP_TIME_HPP_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <ERa/ERaDetect.hpp>
+#include <ERa/ERaTimeLib.hpp>
+#include <Utility/ERaUtility.hpp>
+
+#define ERA_SNTP_TIMEZONE   7L
+#define ERA_SNTP_SERVER1    "vn.pool.ntp.org"
+#define ERA_SNTP_SERVER2    "asia.pool.ntp.org"
+#define ERA_SNTP_SERVER3    "time.nist.gov"
+
+class ERaEspTime
+    : public ERaTime
+{
+public:
+    ERaEspTime()
+        : initialized(false)
+        , timeZone(ERA_SNTP_TIMEZONE)
+        , server1(ERA_SNTP_SERVER1)
+        , server2(ERA_SNTP_SERVER2)
+        , server3(ERA_SNTP_SERVER3)
+    {}
+    virtual ~ERaEspTime()
+    {}
+
+    void config(long tz) {
+        this->timeZone = tz;
+    }
+
+    void config(long tz, const char* sv1, const char* sv2 = NULL,
+                                        const char* sv3 = NULL) {
+        this->timeZone = tz;
+        this->server1 = sv1;
+        this->server2 = sv2;
+        this->server3 = sv3;
+    }
+
+    void begin() override {
+        if (this->initialized) {
+            return;
+        }
+        this->initialized = true;
+        ERaTime::setGetTimeCallback([&, this](void) -> time_t { return this->getTimeRTC(); });
+        ERaTime::setSetTimeCallback([&, this](time_t time) { this->setTimeRTC(time); });
+        configTime(0, 0, this->server1, this->server2, this->server3);
+    }
+
+    void run() override {
+    }
+
+    time_t getTimeRTC() {
+        ::time_t now;
+        ::time(&now);
+        now += (this->timeZone * SECS_PER_HOUR);
+        return (time_t)now;
+    }
+
+    void setTimeRTC(time_t time) {
+        const timeval epoch = {(::time_t)time, 0};
+        settimeofday(&epoch, 0);
+    }
+
+protected:
+private:
+    bool initialized;
+    long timeZone;
+    const char* server1;
+    const char* server2;
+    const char* server3;
+};
+
+#endif /* INC_ERA_ESP_TIME_HPP_ */
